304dc2257916db8a9b1ee7504da507037fdfb5db
yortz <skylarking420@gmail.com>
2013-08-29 14:50:52 +0200

    basic miniserver implementation

---
+++ mini-unicorn.rb
@@ -0,0 +1,25 @@
+require 'socket'
+
+class MiniUnicorn
+  def initialize(port=8080)
+    #socket(2)
+    @listener = Socket.new(:INET, :STREAM)
+
+    #bind(2)
+    @listener.bind(Socket.sockaddr_in(port, '0.0.0.0'))
+
+    #listen(2)
+    @listener.listen(512)
+  end
+
+  def start
+    loop do
+      connection, _ = @listener.accept
+      connection.write(connection.read)
+      connection.close
+    end
+  end
+end
+
+server = MiniUnicorn.new
+server.start
